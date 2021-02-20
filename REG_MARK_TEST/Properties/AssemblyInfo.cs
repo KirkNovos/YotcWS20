b1ab0b18fad6def42b006a9729f74cd096faa370
MaksimMih <vip.peta.1998@mail.ru>
2021-02-20 11:02:22 +0300

    Добавьте файлы проекта.

---
+++ REG_MARK_TEST/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("REG_MARK_TEST")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("REG_MARK_TEST")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c30d2aee-7b32-42e8-9ef5-ff4908d0e0fa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ REG_MARK_TEST/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using RegZnak;
+
+namespace REG_MARK_TEST
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        static REG_MARK reg_mark;
+        [ClassInitialize]
+        static public void Init(TestContext tc)
+        {
+            reg_mark = new REG_MARK();
+        }
+
+        [TestMethod]
+        public void CheckMarkValidation()
+        {
+            Assert.IsTrue(reg_mark.CheckMark("a999aa999"));
+        }
+    }
+}
